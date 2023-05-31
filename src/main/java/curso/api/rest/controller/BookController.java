c22801934347c9d8b89deebf77cc398bb26add6c
bruno-caxa <frian6123@gmail.com>
2023-05-31 15:40:20 -0300

    31/05/23

--- src/main/java/curso/api/rest/controller/BookController.java
+++ src/main/java/curso/api/rest/controller/BookController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import curso.api.rest.model.BookCategory;
 import curso.api.rest.repository.BookCategoryRepository;
 import curso.api.rest.service.BookService;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/api/books")
 public class BookController {
